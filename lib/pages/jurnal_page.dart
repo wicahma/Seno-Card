4108c9b76ed084acbcb133a4d85ca554dfcbd77a
wicahma <teguh.dc.kusuma@gmail.com>
2023-09-06 16:09:55 +0700

    create: main jurnal page

--- lib/pages/jurnal_page.dart
+++ lib/pages/jurnal_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:seno_card/components/bases/base_comp.dart';
-import 'package:seno_card/components/micros/back_button.dart';
+import 'package:seno_card/components/micros/date_chip.dart';
+import 'package:seno_card/components/micros/week_chip.dart';
+import 'package:seno_card/components/micros/jurnal_list.dart';
+import 'package:seno_card/pages/create_jurnal_page.dart';
 
 class JurnalPage extends StatefulWidget {
   const JurnalPage({super.key});
@@ -18,7 +21,13 @@ class _JurnalPageState extends State<JurnalPage> {
           shadowColor: Colors.black.withOpacity(0.3),
           backgroundColor: Colors.white,
           surfaceTintColor: Colors.white,
-          leading: const BackBtn(),
+          leading: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: const Icon(
+                Icons.home_rounded,
+                size: 30,
+                color: Color.fromRGBO(177, 121, 184, 1),
+              )),
           shape: const RoundedRectangleBorder(
               borderRadius:
                   BorderRadius.vertical(bottom: Radius.elliptical(20, 20))),
@@ -30,6 +39,174 @@ class _JurnalPageState extends State<JurnalPage> {
           ),
           centerTitle: true,
         ),
-        child: Center(child: Text('Jurnal Page')));
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+          child: CustomScrollView(
+            clipBehavior: Clip.none,
+            slivers: [
+              SliverToBoxAdapter(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 20),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: const Color.fromRGBO(177, 121, 184, .1),
+                              width: 1),
+                          color: const Color.fromRGBO(247, 233, 248, 1),
+                          borderRadius: BorderRadius.circular(100)),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                        child: Text(
+                          "Hey, Teguh",
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 18, color: Colors.black
+                              //  Color.fromRGBO(177, 121, 184, 1)
+                              ),
+                        ),
+                      ),
+                    ),
+                    const DateChip(),
+                  ],
+                ),
+              ),
+              const SliverToBoxAdapter(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    WeekChip(day: "Sen", date: 1),
+                    WeekChip(day: "Sel", date: 1),
+                    WeekChip(day: "Rab", date: 1),
+                    WeekChip(day: "Kam", date: 1),
+                    WeekChip(day: "Jum", date: 1),
+                    WeekChip(day: "Sab", date: 1),
+                    WeekChip(day: "Min", date: 1),
+                  ],
+                ),
+              ),
+              SliverAppBar.large(
+                expandedHeight: 130,
+                backgroundColor: Colors.transparent,
+                surfaceTintColor: Colors.transparent,
+                pinned: true,
+                floating: false,
+                leading: const SizedBox(),
+                leadingWidth: 0,
+                bottom: const PreferredSize(
+                  preferredSize: Size.fromHeight(60),
+                  child: Text(''),
+                ),
+                clipBehavior: Clip.none,
+                flexibleSpace: Container(
+                  clipBehavior: Clip.antiAlias,
+                  margin: const EdgeInsets.only(top: 10, bottom: 10),
+                  decoration: const BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color.fromRGBO(177, 121, 184, 0.3),
+                          blurRadius: 20,
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  width: double.infinity,
+                  height: 130,
+                  child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      splashColor: const Color.fromRGBO(177, 121, 184, .2),
+                      highlightColor: const Color.fromRGBO(177, 121, 184, .3),
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const CreateJurnalPage())),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Image(
+                              image: AssetImage("assets/images/add-sign.png")),
+                          Text(
+                            "What do you feel today, write here..",
+                            style: TextStyle(
+                                fontFamily: "Quicksand",
+                                fontSize: 12,
+                                color: Colors.grey.shade600),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+                const JurnalList(
+                    dayTime: "2023-08-22 23:30:00.000",
+                    mood: 1,
+                    title: "Tidak bagus sekali hari ini",
+                    description:
+                        "ini adalah sebuah deskripsi panjang yan dibuat berdasarkan random teks yang diketik sendiri yha"),
+              ]))
+            ],
+          ),
+        ));
   }
 }
